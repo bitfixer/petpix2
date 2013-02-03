b83880708da5d8e18b2c3874912af562d049b0fb
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2013-02-03 11:01:21 -0800

    experiments with noise at transmission start

--- PETpix2/PETpix2.c
+++ PETpix2/PETpix2.c
@@ -6,12 +6,74 @@
  */ 
 
 
+#include "UART_routines.h"
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void)
 {
+    uart0_init(25);
+    unsigned char byte;
+    unsigned char buffer[1000];
+    int i;
     while(1)
     {
-        //TODO:: Please write your application code 
+        for (i = 0; i < 1000; i++)
+        {
+            buffer[i] = receiveByte();
+        }
+        
+        /*
+        int res;
+        int start_offset = -1;
+        for (i = 0; i < 1000; i++)
+        {
+            if (start_offset == -1)
+            {
+                if (buffer[i] == 0x00)
+                {
+                    start_offset = i;
+                    transmitString("offset ");
+                    transmitHex(INT, start_offset);
+                    transmitString("\r\n");
+                }
+            }
+            
+            if (start_offset >= 0)
+            {
+                res = (i-start_offset) % 256;
+
+                if (res != buffer[i])
+                {
+                    transmitString("error:");
+                    transmitHex(INT, i);
+                    transmitByte(' ');
+                    transmitHex(CHAR, buffer[i]);
+                    transmitByte(' ');
+                    transmitHex(INT, res);
+                    transmitString("\r\n");
+                }
+            }
+            
+        }
+        */
+        
+        for (i = 0; i < 100; i++)
+        {
+            transmitHex(CHAR, buffer[i]);
+            transmitString(" ");
+            transmitHex(INT, i);
+            
+            if (i > 0)
+            {
+                if (buffer[i] != buffer[i-1]+1)
+                {
+                    transmitString(" **");
+                }
+            }
+            
+            transmitString("\r\n");
+            _delay_loop_2(65535);
+        }
     }
 }
