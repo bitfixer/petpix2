47d943f8c98765d2874c384c46596f333bb5a84d
Michael Hill <gubbish@Michaels-MacBook-Pro.local>
2013-02-02 21:22:48 -0800

    add initial source file

---
+++ PETpix2/PETpix2.c
@@ -0,0 +1,17 @@
+/*
+ * PETpix2.c
+ *
+ * Created: 2/2/2013 9:14:06 PM
+ *  Author: Michael Hill
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
